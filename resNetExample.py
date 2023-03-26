2e54187ac9d06ce6bb0e15996b479183d5a635e5
Nerea Leon <82573517+nlm18@users.noreply.github.com>
2023-03-27 00:52:05 +0200

    Ejemplo funcional con resnet50 sin usar kerasClassifier, con accuracy ~80%

--- resNetExample.py
+++ resNetExample.py
@@ -1,117 +1,170 @@
-import tensorflow as tf
-
-tf.compat.v1.disable_eager_execution()
-from tensorflow import keras
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.losses import categorical_crossentropy
+#https://www.kaggle.com/code/kutaykutlu/resnet50-transfer-learning-cifar-10-beginner
+import os, re, time, json
+import PIL.Image, PIL.ImageFont, PIL.ImageDraw
 import numpy as np
-import matplotlib.pyplot as plt
-
-from keras.datasets import cifar10 as cifar10
-from art.attacks.evasion import FastGradientMethod
-from art.estimators.classification import KerasClassifier
-#las x son las imagenes y las 'y' los label de las imagenes 'x'
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-# Input image dimensions.
-input_shape = x_train.shape[1 :]
-num_classes=10
 
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-# Convert class vectors to binary class matrices.
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.optimizers import Adam
+from matplotlib import pyplot as plt
+import tensorflow_datasets as tfds
+#tf.compat.v1.disable_eager_execution()#si no lo pongo sale error
 
-# Print Training and Test Samples
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-print('y_train shape:', y_train.shape)
+BATCH_SIZE = 32
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
-model = Sequential()
-pretrained_model = tf.keras.applications.ResNet50(
-    include_top=False,
-    weights="imagenet",
-    input_tensor=None,
-    input_shape=input_shape,
-    pooling=None,
-    classes=num_classes
-)
-for layer in pretrained_model.layers:
-    layer.trainable = False
-model.add(pretrained_model)
-model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
+# Matplotlib config
+plt.rc('image', cmap='gray')
+plt.rc('grid', linewidth=0)
+plt.rc('xtick', top=False, bottom=False, labelsize='large')
+plt.rc('ytick', left=False, right=False, labelsize='large')
+plt.rc('axes', facecolor='F8F8F8', titlesize="large", edgecolor='white')
+plt.rc('text', color='a8151a')
+plt.rc('figure', facecolor='F0F0F0')  # Matplotlib fonts
+MATPLOTLIB_FONT_DIR = os.path.join(os.path.dirname(plt.__file__), "mpl-data/fonts/ttf")
 
-model.compile(loss=categorical_crossentropy, optimizer=Adam(learning_rate=0.001), metrics=["accuracy"])
-if x_train.min() < x_test.min():
-    min_pixel_value = x_train.min()
-else:
-    min_pixel_value = x_test.min()
-if x_train.max() > x_test.max():
-    max_pixel_value = x_train.max()
-else:
-    max_pixel_value = x_test.max()
 
-classifier = KerasClassifier(model=model, clip_values=(min_pixel_value, max_pixel_value), use_logits=False)
-epochs = 3
-classifier.fit(x_train, y_train, batch_size=64, nb_epochs=epochs)
+# utility to display a row of digits with their predictions
+def display_images(digits, predictions, labels, title) :
+    n = 10
 
-predictions = classifier.predict(x_test)
-accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(y_test, axis=1)) / len(y_test)
+    indexes = np.random.choice(len(predictions), size=n)
+    n_digits = digits[indexes]
+    n_predictions = predictions[indexes]
+    n_predictions = n_predictions.reshape((n,))
+    n_labels = labels[indexes]
+
+    fig = plt.figure(figsize=(20, 4))
+    plt.title(title)
+    plt.yticks([])
+    plt.xticks([])
+
+    for i in range(10):
+        ax = fig.add_subplot(1, 10, i + 1)
+        class_index = n_predictions[i]
+
+        plt.xlabel(classes[class_index])
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(n_digits[i])
+
+# utility to display training and validation curves
+def plot_metrics(metric_name, title, ylim=5) :
+    fig1 = plt.gcf()
+    plt.title(title)
+    plt.ylim(0, ylim)
+    plt.plot(history.history[metric_name], color='blue', label=metric_name)
+    plt.plot(history.history['val_' + metric_name], color='green', label='val_' + metric_name)
+    plt.ylabel(metric_name)
+    plt.xlabel('Epochs')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+#Loading and preprocessing data
+(training_images, training_labels) , (validation_images, validation_labels) = tf.keras.datasets.cifar10.load_data()
+display_images(training_images, training_labels, training_labels, "Training Data" )
+#plt.show()
+def preprocess_image_input(input_images):
+  input_images = input_images.astype('float32')
+  output_ims = tf.keras.applications.resnet50.preprocess_input(input_images)
+  return output_ims
+
+train_X = preprocess_image_input(training_images)
+valid_X = preprocess_image_input(validation_images)
+
+#print( train_X[0].shape)
+'''
+Feature Extraction is performed by ResNet50 pretrained on imagenet weights. 
+Input size is 224 x 224.
+'''
+def feature_extractor(inputs):
+    feature_extractor = tf.keras.applications.resnet.ResNet50(input_shape=(224, 224, 3),
+                                                              include_top=False,
+                                                              weights='imagenet')(inputs)
+    return feature_extractor
+
+
+'''
+Defines final dense layers and subsequent softmax layer for classification.
+'''
+
+def classifier(inputs):
+    x = tf.keras.layers.GlobalAveragePooling2D()(inputs)
+    x = tf.keras.layers.Flatten()(x)
+    x = tf.keras.layers.Dense(1024, activation="relu")(x)
+    x = tf.keras.layers.Dense(512, activation="relu")(x)
+    x = tf.keras.layers.Dense(10, activation="softmax", name="classification")(x)
+    return x
+
+
+'''
+Since input image size is (32 x 32), first upsample the image by factor of (7x7) to transform it to (224 x 224)
+Connect the feature extraction and "classifier" layers to build the model.
+'''
+
+
+def final_model(inputs):
+    resize = tf.keras.layers.UpSampling2D(size=(7, 7))(inputs)
+    print(resize)
+    resnet_feature_extractor = feature_extractor(resize)
+    classification_output = classifier(resnet_feature_extractor)
+
+    return classification_output
+
+
+'''
+Define the model and compile it. 
+Use Stochastic Gradient Descent as the optimizer.
+Use Sparse Categorical CrossEntropy as the loss function.
+'''
+
+
+def define_compile_model():
+    inputs = tf.keras.layers.Input(shape=(32, 32, 3))
+
+    classification_output = final_model(inputs)
+    model = tf.keras.Model(inputs=inputs, outputs=classification_output)
+    for layer in model.layers[:-5]:
+        layer.trainable = False
+    model.compile(optimizer='Adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    return model
+
+
+model = define_compile_model()
+#model.summary()
+
+EPOCHS = 3
+history = model.fit(train_X, training_labels, epochs=EPOCHS, validation_data = (valid_X, validation_labels), batch_size=64)
+
+#model.save("cifar10_resnet50") no se guarda el history?
+loss, accuracy = model.evaluate(valid_X, validation_labels, batch_size=64)
+plt.clf()
+plot_metrics("loss", "Loss", 1)
+plt.clf()
+plot_metrics("accuracy", "Accuracy", 1.5)
+
+probabilities = model.predict(valid_X, batch_size=64)
+probabilities = np.argmax(probabilities, axis = 1)
+
+plt.clf()
+display_images(validation_images, probabilities, validation_labels, "Predictions indicated in red.")
+plt.show()
+
+'''
+
+accuracy = np.sum(probabilities == np.argmax(validation_labels, axis=1)) / len(validation_labels)
 print("Accuracy on benign test examples: {}%".format(accuracy * 100))
 
-'''w =tf.shape(x_train)
-model.fit(tf.reshape(x_train,(w[0],32,32,3)), y_train, epochs=5, steps_per_epoch=100 )
-#model.fit(x_train, y_train, epochs=5)
+from art.attacks.evasion import FastGradientMethod
+# Generate adversarial test examples
+attack = FastGradientMethod(estimator=classifier, eps=0.2)
+x_test_adv = attack.generate(x=valid_X)
 
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
-
-
-history = model.fit(x_train, y_train, validation_split=0.3, epochs=epochs, batch_size= 32, shuffle=True)#validation_data=(x,y)
-#lista de datos del historial
-#print(history.history.keys())
-
-scores = model.evaluate(x_test, y_test, verbose = 1)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
-
-fig1= plt.gcf()
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.grid()
-plt.title('ResNet model accuracy')
-plt.ylabel('Accuracy')
-plt.xlabel('Epochs')
-plt.legend(['train', 'validation'], loc='upper left')
-plt.show()
-
-fig2= plt.gcf()
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.grid()
-plt.title('ResNet model loss')
-plt.ylabel('Loss')
-plt.xlabel('Epochs')
-plt.legend(['train', 'validation'], loc='upper left')
-plt.show()
-
-class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(x_test[i])
-    # The CIFAR labels happen to be arrays,
-    # which is why you need the extra index
-    plt.xlabel(class_names[y_test[i][0]])
-plt.show()'''
+# Evaluate the ART classifier on adversarial test examples
+predictions = classifier.predict(x_test_adv)
+accuracy = np.sum(np.argmax(predictions, axis=1) == np.argmax(validation_labels, axis=1)) / len(validation_labels)
+print("Accuracy on adversarial test examples: {}%".format(accuracy * 100))'''
