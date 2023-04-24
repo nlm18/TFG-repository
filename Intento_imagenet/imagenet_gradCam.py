8430e258008f9fe815f7e987e24223ef868a0c8f
Nerea Leon <82573517+nlm18@users.noreply.github.com>
2023-04-25 00:19:48 +0200

    Se han corregido los errores de memoria y 'NoneType' is not callable. Se ha guardado en una variable el X_test

--- Intento_imagenet/imagenet_gradCam.py
+++ Intento_imagenet/imagenet_gradCam.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-from keras.applications.resnet import preprocess_input, decode_predictions
+from keras.applications.efficientnet import preprocess_input, decode_predictions
     #Xception, preprocess_input, decode_predictions
 import random
 from keras.utils import np_utils
@@ -18,8 +18,16 @@ import time
 #Mas ataques: https://adversarial-robustness-toolbox.readthedocs.io/en/latest/modules/attacks/evasion.html#fast-gradient-method-fgm
 from art.attacks.evasion import FastGradientMethod, BasicIterativeMethod, ProjectedGradientDescent
 from art.estimators.classification import TensorFlowV2Classifier
+import pickle
 
 # ------------------------ Funciones auxiliares ---------------------------------
+def guardar_datos(datos, filename):
+    with open(filename, "wb") as f:
+        pickle.dump(datos, f)
+
+def cargar_datos(filename):
+     with open(filename, "rb") as f:
+         return pickle.load(f)
 def train_step(model, images, labels):
     with tf.GradientTape() as tape:
         predictions = model(images, training=True)
@@ -29,19 +37,20 @@ def train_step(model, images, labels):
 
 def getAttackMethod(name, classifier, epsilon):
     if name == 'FastGradientMethod':
-        return FastGradientMethod(estimator=classifier, eps=epsilon)
+        return FastGradientMethod(estimator=classifier, eps=epsilon, batch_size=4)
     elif name == 'BasicIterativeMethod':
-        return BasicIterativeMethod(estimator=classifier, eps=epsilon, eps_step=0.5, max_iter=15)
+        return BasicIterativeMethod(estimator=classifier, eps=epsilon, max_iter=15, batch_size=10)
     elif name == 'ProjectedGradientDescent':
-        return ProjectedGradientDescent(estimator=classifier, eps=epsilon, eps_step=0.5, max_iter=15)
+        return ProjectedGradientDescent(estimator=classifier, eps=epsilon, max_iter=15, batch_size=10)
 
-def executeGradCam(num, epsilon, n_iter):
+def executeGradCam(num, classifier, epsilon, n_iter):
     # https://stackoverflow.com/questions/66182884/how-to-implement-grad-cam-on-a-trained-network
     # Prepare image
     img_orig = X_test[num]
-    list_img = [img_orig]
-    for i in range(0, len(epsilon)):
-        list_img.append(x_test_adv[i+n_iter*len(epsilon)][num])
+    list_img = []
+    list_img.append(img_orig)
+    for j in range(0, len(epsilon)):
+        list_img.append(x_test_adv[j+n_iter*len(epsilon)][num])
     img_array = []
     preds = []
     predicted = []
@@ -53,8 +62,6 @@ def executeGradCam(num, epsilon, n_iter):
     for ind in range(0, len(list_img)): #ind == 0 es la imagen sin modificar
         img_array.append(gradCamInterface.get_img_array(list_img[ind]))
         preds.append(classifier.predict(img_array[ind]))
-        predicted.append(decode_predictions(preds[ind]))
-        real_value = decode_predictions(preds[ind])
 
         # Generate class activation heatmap
         heatmap.append(gradCamInterface.make_gradcam_heatmap(img_array[ind], model, last_conv_layer_name))
@@ -65,10 +72,12 @@ def executeGradCam(num, epsilon, n_iter):
         gradCam_img.append(gradCamInterface.display_gradcam(img_255[ind], heatmap[ind]))
 
         if ind == 0:
-            print("Predicted benign example: ", predicted[ind])
+            predicted = decode_predictions(preds[ind], top=1)
+            print("Predicted benign example: ", predicted[ind][0][1])
         else:
+            predicted.append(decode_predictions(preds[ind], top=1))
             print("AttackMethod: %s with epsilon = %s" % (attackName[atck], epsilon[ind-1]))
-            print("Predicted adversarial example: ", predicted[ind], "   real value: ", real_value)
+            print("Predicted adversarial example: ", predicted[ind][0][0][1])
 
         plot_img.append(keras.preprocessing.image.array_to_img(img_255[ind]))
         plot_img.append(gradCam_img[ind])
@@ -80,7 +89,6 @@ def save_and_plot_results(num, list_of_images, predicted, epsilon, attack):
     fig, axs = plt.subplots(nrows=num_rows, ncols=2, figsize=(15, 15), subplot_kw={'xticks': [], 'yticks': []}, layout='compressed')
     ind = 0;
     ind_pred = 0;
-    real_value = gradCamInterface.decode_predictions(y_test[num], num_classes, classes)
     for ax in axs.flat:
         ax.imshow(list_of_images[ind])
         #Ponemos titulos y nombre de los ejes
@@ -96,65 +104,82 @@ def save_and_plot_results(num, list_of_images, predicted, epsilon, attack):
             ax.set_title('GradCam')
             ind_pred+=1
         ind+=1
-    suptitle = 'Real value: %s, attack method used: %s' % (real_value, attack)
+    suptitle = 'Attack method used: %s' % (attack)
     fig.suptitle(suptitle)
     try :
         os.mkdir('gradCam_examples_attack_method-%s' % (attack))
     except OSError as e :
         if e.errno != errno.EEXIST :
             raise
-    File_name = 'gradCam_examples_attack_method-%s/gradCam_example_image-%s_attack_method-%s_Real-%s.jpg' % (attack, num, attack, real_value)
+    File_name = 'gradCam_examples_attack_method-%s/gradCam_example_image-%s_attack_method-%s.jpg' % (attack, num, attack )
     fig.savefig(File_name)
 
 # ------------------------ Código principal ---------------------------------
 #%% Data Preparation
 # Load data   t1 = time.time()
 num_classes = 1000
-input_images_path = "C:/Users/User/TFG-repository/EjemploFuncional_imagenet/imagenes/"
-files_names = os.listdir(input_images_path)
-img_size = (224, 224)
+img_size = (300, 300)
 A = []
 i = 0
 n = 25
+total_img=47
 while(i < n):
-    A.append(random.randint(0, 47))#4434
+    A.append(random.randint(0, total_img))#4434
     i+=1
 # Load model
-shape = (224, 224, 3)
-model = tf.keras.applications.resnet.ResNet50(weights="imagenet", include_top=False, classes=num_classes, input_shape=shape)
+img_shape = (300, 300, 3)
+model = tf.keras.applications.EfficientNetB3(weights="imagenet", include_top=True, classes=num_classes, input_shape=img_shape)
 model.trainable = False
 lr=0.001
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
-classifier = TensorFlowV2Classifier(model=model, clip_values=(0, 1), nb_classes=num_classes, input_shape=shape, loss_object=loss_object, train_step=train_step)
+classifier = TensorFlowV2Classifier(model=model, clip_values=(0, 1), nb_classes=num_classes, input_shape=img_shape, loss_object=loss_object, train_step=train_step)
 
-
-X_test = np.ndarray(shape=(50, 224, 224, 3), dtype='float32')
-img_path =[]
-for index in range(0, n):
-    # Preprocess data
-    img_path.append(input_images_path+files_names[A[index]])
-    X_test[index] = preprocess_input(gradCamInterface.get_img_array_path(img_path[index], img_size))
+#Load Images
+X_test = np.ndarray(shape=(total_img+1, 300, 300, 3), dtype='float32')
+loadImages = False
+if loadImages== True:
+    input_images_path = "C:/Users/User/TFG-repository/Intento_imagenet/imagenes/"
+    files_names = os.listdir(input_images_path)
+    img_path =[]
+    for index in range(0, total_img):
+        # Preprocess data
+        img_path.append(input_images_path+files_names[index])
+        X_test[index] = preprocess_input(gradCamInterface.get_img_array_path(img_path[index], img_size))
+    guardar_datos(X_test, "testImages.pkl")
+else:
+    X_test = cargar_datos("testImages.pkl")
 
 
 # Para distintos valores de epsilon
-epsilon = [0.01]#[0.01, 0.05, 0.1, 0.15]
+loadImages = True
+epsilon = [1]#[0.01, 0.05, 0.1, 0.15]
 x_test_adv = []
 attackName = ['FastGradientMethod']# ['FastGradientMethod', 'BasicIterativeMethod', 'ProjectedGradientDescent']
-for atck in range(0, len(attackName)):
-    for i in range(0, len(epsilon)):
-        # Generate adversarial test examples
-        attack = getAttackMethod(attackName[atck], classifier, epsilon[i])
-        x_test_adv.append(attack.generate(x=X_test))
+if loadImages == True:
+    for atck in range(0, len(attackName)):
+        for i in range(0, len(epsilon)):
+            # Generate adversarial test examples
+            attack = getAttackMethod(attackName[atck], classifier, epsilon[i])
+            x_test_adv.append(attack.generate(x=X_test))
+        filename = "Adv_Images_AttackMethod_" + attackName[atck] +"_Epsilon_%s" % (epsilon) + ".pkl"
+        guardar_datos(x_test_adv, filename)
+else:
+    for atck in range(0, len(attackName)):
+        filename = "Adv_Images_AttackMethod_" + attackName[atck] +"_Epsilon_%s" % (epsilon) + ".pkl"
+        if atck == 0:
+            x_test_adv = cargar_datos(filename)
+        else:
+            x_test_adv.append(cargar_datos(filename))
 
 #GRAD CAM
 # Remove last layer's softmax
 model.layers[-1].activation = None
-last_conv_layer_name = "block14_sepconv2_act"
+last_conv_layer_name = "top_activation"
 
 for atck in range(0, len(attackName)):
     for index in range(0, n):
-        list_of_images, predicted = executeGradCam(A[index], epsilon, atck)
+        list_of_images, predicted = executeGradCam(A[index], classifier, epsilon, atck)
         save_and_plot_results(A[index], list_of_images, predicted, epsilon, attackName[atck])
 
 #https://stackoverflow.com/questions/66182884/how-to-implement-grad-cam-on-a-trained-network
