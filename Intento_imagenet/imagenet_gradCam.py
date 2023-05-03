0588300c1437ee71ead635f720aeb4b2af3e1e48
Nerea Leon <82573517+nlm18@users.noreply.github.com>
2023-05-03 22:36:33 +0200

    Se ha añadido la visualizacion de la diferencia entre la imagen original y la adversaria. Se han metido dos tipos mas de ataque, tardan mucho en generar las imagenes.

--- Intento_imagenet/imagenet_gradCam.py
+++ Intento_imagenet/imagenet_gradCam.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
 import art.attacks.evasion
-
+import math
 import gradCamInterface
 import os
 import errno
@@ -16,7 +16,7 @@ from keras.utils import np_utils
 import time
 
 #Mas ataques: https://adversarial-robustness-toolbox.readthedocs.io/en/latest/modules/attacks/evasion.html#fast-gradient-method-fgm
-from art.attacks.evasion import FastGradientMethod, BasicIterativeMethod, ProjectedGradientDescent
+from art.attacks.evasion import FastGradientMethod, BasicIterativeMethod, ProjectedGradientDescent, CarliniLInfMethod, HopSkipJump
 from art.estimators.classification import TensorFlowV2Classifier
 import pickle
 
@@ -36,12 +36,17 @@ def train_step(model, images, labels):
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
 def getAttackMethod(name, classifier, epsilon):
+#attackName = ['FastGradientMethod', 'BasicIterativeMethod', 'ProjectedGradientDescent', 'CarliniLInfMethod', 'HopSkipJump']
     if name == 'FastGradientMethod':
         return FastGradientMethod(estimator=classifier, eps=epsilon, batch_size=4)
     elif name == 'BasicIterativeMethod':
-        return BasicIterativeMethod(estimator=classifier, eps=epsilon, max_iter=15, batch_size=10)
+        return BasicIterativeMethod(estimator=classifier, eps=epsilon, max_iter=100, batch_size=4)
     elif name == 'ProjectedGradientDescent':
-        return ProjectedGradientDescent(estimator=classifier, eps=epsilon, max_iter=15, batch_size=10)
+        return ProjectedGradientDescent(estimator=classifier, eps=epsilon, max_iter=100, batch_size=4)
+    elif name == 'CarliniLInfMethod':
+        return CarliniLInfMethod(classifier=classifier, confidence=epsilon, learning_rate=0.2, max_iter=10, batch_size=4)
+    elif name == 'HopSkipJump':
+        return HopSkipJump(classifier=classifier, max_iter=50, batch_size=4)
 
 def executeGradCam(num, classifier, epsilon, n_iter):
     # https://stackoverflow.com/questions/66182884/how-to-implement-grad-cam-on-a-trained-network
@@ -117,42 +122,67 @@ def save_and_plot_results(num, list_of_images, predicted, epsilon, attack):
     File_name = 'gradCam_examples_attack_method-%s/gradCam_example_image-%s_attack_method-%s.jpg' % (attack, num, attack)
     fig.savefig(File_name)
 
+def plot_difference(num, original_img, adversarial_img, n_iter, attack):
+    div_entera = (len(epsilon) % 2 == 0)
+    num_col = 1
+    num_rows = len(epsilon)
+    if div_entera:
+        num_col = 2
+        num_rows = math.ceil(len(epsilon) / 2)
+    for j in range(0, len(epsilon)):
+        resultado = (abs(original_img[num]-adversarial_img[j+n_iter*len(epsilon)][num]))*255
+        #Ponemos titulo
+        plt.subplot(num_rows, num_col, j + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.xlabel('Adv-Orig, $\epsilon$=%s'% (epsilon[j]))
+        plt.imshow(resultado.astype(np.uint8))
+    suptitle = 'Attack method used: %s' % (attack)
+    plt.suptitle(suptitle)
+    try :
+        os.mkdir('Difference_between_orig_adv_method-%s' % (attack))
+    except OSError as e :
+        if e.errno != errno.EEXIST :
+            raise
+    File_name = 'Difference_between_orig_adv_method-%s/Difference_image-%s_attack_method-%s.jpg' % (attack, num, attack)
+    plt.savefig(File_name)
 # ------------------------ Código principal ---------------------------------
 #%% Data Preparation
 # Load data   t1 = time.time()
 num_classes = 1000
-img_size = (300, 300)
+img_size = (224, 224)
 A = []
 i = 0
 
 imagenet = False
 if imagenet :
-    input_images_path = "C:/Users/User/TFG-repository/EjemploFuncional_imagenet/ILSVRC2012_img_val/"
+    input_images_path = "C:/Users/User/TFG-repository/Intento_imagenet/ILSVRC2012_img_val/"
     imagenet_txt = "_imagenet"
     n = 50
     total_img=4434
 else :
-    input_images_path = "C:/Users/User/TFG-repository/EjemploFuncional_imagenet/imagenes/"
+    input_images_path = "C:/Users/User/TFG-repository/Intento_imagenet/imagenes/"
     imagenet_txt = ""
     n = 25
-    total_img=46
+    total_img=48
 while(i < n):
-    A.append(random.randint(0, total_img))
+    A.append(random.randint(0, total_img-1))
     i+=1
 # Load model
-img_shape = (300, 300, 3)
-model = tf.keras.applications.EfficientNetB3(weights="imagenet", include_top=True, classes=num_classes, input_shape=img_shape)
+img_shape = (224, 224, 3)
+#EfficientNetB3
+model = tf.keras.applications.EfficientNetB0(weights="imagenet", include_top=True, classes=num_classes, input_shape=img_shape)
 model.trainable = False
-lr=0.001
+lr=0.01
 optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
 classifier = TensorFlowV2Classifier(model=model, clip_values=(0, 1), nb_classes=num_classes, input_shape=img_shape, loss_object=loss_object, train_step=train_step)
 
 #Load Images
-X_test = np.ndarray(shape=(total_img+1, 300, 300, 3), dtype='float32')
-loadImages = False
+X_test = np.ndarray(shape=(total_img, 224, 224, 3), dtype='float32')
+createImages = False #Si está a true cargará las imagenes originales desde la carpeta y generará las adversarias
 
-if loadImages== True:
+if createImages== True:
     files_names = os.listdir(input_images_path)
     img_path =[]
     for index in range(0, total_img):
@@ -165,18 +195,26 @@ else:
 
 
 # Para distintos valores de epsilon
-loadImages = True
-epsilon = [0.4, 0.5, 0.6, 0.7]#[0.01, 0.05, 0.1, 0.15]
+createImages = False
+epsilon = [0.2, 0.4]#[0.01, 0.05, 0.1, 0.15]
 x_test_adv = []
-attackName = ['FastGradientMethod']# ['FastGradientMethod', 'BasicIterativeMethod', 'ProjectedGradientDescent']
-if loadImages == True:
+attackName = ['HopSkipJump']
+X_test = X_test[0:5]
+if createImages == True:
     for atck in range(0, len(attackName)):
+        individual_atck = []
         for i in range(0, len(epsilon)):
+            if attackName == 'HopSkipJump' and i >0:
+                filename = "Adv_Images_AttackMethod_" + attackName[atck] + "max_iter_50.pkl"
+                guardar_datos(individual_atck, filename)
+            else:
             # Generate adversarial test examples
-            attack = getAttackMethod(attackName[atck], classifier, epsilon[i])
-            x_test_adv.append(attack.generate(x=X_test))
+                attack = getAttackMethod(attackName[atck], classifier, epsilon[i])
+                x_test_adv.append(attack.generate(x=X_test))
+                individual_atck.append(attack.generate(x=X_test))
         filename = "Adv_Images_AttackMethod_" + attackName[atck] +"_Epsilon_%s" % (epsilon) + ".pkl"
-        guardar_datos(x_test_adv, filename)
+        guardar_datos(individual_atck, filename)
+    guardar_datos(x_test_adv, "atcks_%s" % (attackName) +"_Epsilon_%s" % (epsilon) + ".pkl")
 else:
     for atck in range(0, len(attackName)):
         filename = "Adv_Images_AttackMethod_" + attackName[atck] +"_Epsilon_%s" % (epsilon) + ".pkl"
@@ -188,11 +226,12 @@ else:
 #GRAD CAM
 # Remove last layer's softmax
 model.layers[-1].activation = None
-last_conv_layer_name = "top_activation"
+last_conv_layer_name = "top_activation"#block7a_activation
 
 for atck in range(0, len(attackName)):
-    for index in range(0, n):
-        list_of_images, predicted = executeGradCam(A[index], classifier, epsilon, atck)
-        save_and_plot_results(A[index], list_of_images, predicted, epsilon, attackName[atck])
+    for index in range(0, 5):
+        list_of_images, predicted = executeGradCam(index, classifier, epsilon, atck)
+        save_and_plot_results(index, list_of_images, predicted, epsilon, attackName[atck])
+        plot_difference(index, X_test, x_test_adv, atck, attackName[atck])
 
 #https://stackoverflow.com/questions/66182884/how-to-implement-grad-cam-on-a-trained-network
