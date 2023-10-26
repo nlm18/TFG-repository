8644abaf5b31aea644de6cfac165361bef42819d
Nerea Leon <82573517+nlm18@users.noreply.github.com>
2023-10-26 23:44:00 +0200

    Se ha reordenado los parametros  del objeto imagen y se ha añadido uno nuevo para guardar cual es la imagen original mas cercana al adv natural

--- Imagenet/Imagen.py
+++ Imagenet/Imagen.py
@@ -1,39 +1,43 @@
 from idDict import idDictionary
 class Imagen:
-    def __init__(self, imageName, imageData, imageSize, imageId, networkName, attackName='', epsilon='', isAdvNatural=False):
+    def __init__(self, imageName, imageData, imageSize, imageId, networkName):
         self.name = imageName
         self.data = imageData
-        self.heatmap = 0
         self.size = imageSize #Indica el tamaño de la imagen (Ej:(224, 224))
         self.id = imageId #Indica el ID (n--------) de la clase real del objeto
+        self.networkModelName = networkName #Indica el nombre de la red neuronal con el que se hizo la prediccion de los datos
         self.idName = idDictionary[imageId] #Indica el nombre de la clase real del objeto
+        self.heatmap = 0
         self.predictionId = ''
         self.predictionName = ''
-        self.attackName = attackName #Indica el metodo de ataque con el cual se ha generado la imagen
-        self.epsilon = epsilon #Indica el epsilon del metodo de ataque con el cual se ha generado la imagen
-        self.advNatural = isAdvNatural #Indica si es un adversario natural
-        self.networkModelName = networkName #Indica el nombre de la red neuronal con el que se hizo la prediccion de los datos
-
-    def addPrediction(self, predID):
-        self.predictionId = predID
-        self.predictionName = idDictionary[predID]
-
-    def modifyData(self, imageData):
-        self.data = imageData
-
-    def addAdversarialData(self, attackName, epsilon):
-        self.attackName = attackName
-        self.epsilon = epsilon
+        self.attackName = '' #Indica el metodo de ataque con el cual se ha generado la imagen
+        self.epsilon = '' #Indica el epsilon del metodo de ataque con el cual se ha generado la imagen
+        self.advNatural = False #Indica si es un adversario natural
+        self.closerOriginalImageName = '' #Indica el nombre de la red neuronal con el que se hizo la prediccion de los datos
 
     def copyImage(self):
         img = Imagen(self.name, self.data, self.size, self.id, self.networkModelName)
         return img
 
+    def modifyData(self, imageData):
+        self.data = imageData
+
+    def addNetworkModelName(self, networkName) :
+        self.networkModelName = networkName
+
     def addHeatmap(self, heatmapData):
         self.heatmap = heatmapData
+
+    def addPrediction(self, predID):
+        self.predictionId = predID
+        self.predictionName = idDictionary[predID]
+
+    def addAdversarialData(self, attackName, epsilon):
+        self.attackName = attackName
+        self.epsilon = epsilon
 
     def addAdvNatural(self, isAdvNatural):
         self.advNatural = isAdvNatural
 
-    def addNetworkModelName(self, networkName):
-        self.networkModelName = networkName
+    def addCloserOriginalImageName(self, closerOriginalImageName) :
+        self.closerOriginalImageName = closerOriginalImageName
