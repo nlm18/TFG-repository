010517df824a4d6cb43bdbd64e2c31af3685ffa1
Nerea Leon <82573517+nlm18@users.noreply.github.com>
2023-10-28 12:56:09 +0200

    comentario actualizado del ultimo parametro

--- Imagenet/Imagen.py
+++ Imagenet/Imagen.py
@@ -13,8 +13,8 @@ class Imagen:
         self.attackName = '' #Indica el metodo de ataque con el cual se ha generado la imagen
         self.epsilon = '' #Indica el epsilon del metodo de ataque con el cual se ha generado la imagen
         self.advNatural = False #Indica si es un adversario natural
-        self.closerOriginalImageName = '' #Indica el nombre de la red neuronal con el que se hizo la prediccion de los datos
-
+        self.closerOriginalImageName = '' #Indica el nombre de imagen original mas cercana en el caso de que sea adv natural
+        
     def copyImage(self):
         img = Imagen(self.name, self.data, self.size, self.id, self.networkModelName)
         return img
