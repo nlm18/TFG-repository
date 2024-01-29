765e2c08c2f9062d99d289f103506f04b0edae5b
Nerea Leon <82573517+nlm18@users.noreply.github.com>
2024-01-29 23:05:42 +0100

    Se controla que se duplique la variable para no pisar valores

--- Imagenet/main_OneByOne.py
+++ Imagenet/main_OneByOne.py
@@ -6,6 +6,7 @@ from selectOrigImages import searchCloserImageByID
 
 import os
 import errno
+import copy
 import tensorflow as tf
 from tensorflow import keras
 #Mas ataques: https://adversarial-robustness-toolbox.readthedocs.io/en/latest/modules/attacks/evasion.html#fast-gradient-method-fgm
@@ -33,11 +34,12 @@ def executeGradCam(orig, adv) :
     # Generate class activation heatmap
     for ind in range(0, len(list_img)):
         img_array = gradCamInterface.get_img_array(list_img[ind].data)
-        heatmap = gradCamInterface.make_gradcam_heatmap(img_array, model, last_conv_layer_name)
+        auxi = aux.preprocess_input(list_img[0].networkModelName, copy.deepcopy(img_array))#test
+        heatmap = gradCamInterface.make_gradcam_heatmap(auxi, model, last_conv_layer_name)
         list_img[ind].addHeatmap(heatmap)
 
         # Display heatmap. Ya esta entre 0-255
-        gradCam_img = gradCamInterface.display_gradcam(list_img[ind].data, heatmap)
+        gradCam_img = gradCamInterface.display_gradcam(list_img[ind].data, heatmap, alpha=0.4)
 
         plot_img.append(keras.preprocessing.image.array_to_img(list_img[ind].data))
         plot_img.append(gradCam_img)
@@ -55,13 +57,14 @@ IMG_SHAPE = (299, 299, 3)
 LR = 0.01 #Learning Rate usado en el optimizador
 NUM_IMG = 500 #Cantidad de imagenes de test
 TOTAL_IMG = 500 #Cantidad de imagenes de las que se disponen, imagenet=50000
-ATTACK_NAME = 'FastGradientMethod'
-NetworkModelName = 'EfficientNetB0'
+ATTACK_NAME = 'HopSkipJump'#ProjectedGradientDescent FastGradientMethod BasicIterativeMethod HopSkipJump CarliniLInfMethod
+#comprobar epsilon en el adv
+NetworkModelName = 'Xception'
 #cambiar parametros de entrada de loadImages segun si son de imagenet o no
-IMG_PATH = "C:/Users/User/TFG-repository/webcam_gradcam/ImageNetWebcam/luz_validos/waterBottle_%s_5000luz/" % (NetworkModelName)
-folder = 'frames_naturalAdv/'#'frames_naturalAdv/' 'selectedOrigImages/'
+IMG_PATH = "C:/Users/User/TFG-repository/webcam_gradcam/ImageNetWebcam/luz_validos/waterBottle_%s_5000luz/" % (NetworkModelName) #/luz_validos/waterBottle_%s_5000luz/ /FountainPen_%s_5000name/
+folder = 'selectedOrigImages/'#'frames_naturalAdv/' 'selectedOrigImages/'
 EXECUTION_ID = "Test_%s" % (NetworkModelName)#Se usará para no sustituir variables de distintas ejecuciones
-realID='n04557648'
+realID='n04557648'#'n04557648' botella de agua ------ n03388183 fountain pen
 
 if NetworkModelName == "vgg16" or NetworkModelName == "VGG16" or NetworkModelName == "EfficientNetB0" or NetworkModelName == "mobileNet" or NetworkModelName == "MobileNet" :
     IMG_SIZE = (224, 224)
@@ -72,13 +75,14 @@ model = aux.getNetworkModel(NetworkModelName)
 model.trainable = False
 optimizer = tf.keras.optimizers.Adam(learning_rate=LR)
 loss_object = tf.keras.losses.CategoricalCrossentropy(from_logits=False)
-classifier = TensorFlowV2Classifier(model=model, clip_values=(0, 1), nb_classes=NUM_CLASSES, input_shape=IMG_SHAPE, loss_object=loss_object, train_step=train_step)
+classifier = TensorFlowV2Classifier(model=model, clip_values=(0, 255), nb_classes=NUM_CLASSES, input_shape=IMG_SHAPE, loss_object=loss_object, train_step=train_step)
 
 aux.createDirs(EXECUTION_ID, onebyone=True)
 num_AdvNaturales = 0
 startIndex = 0
 #Load Images
 for num_img in range(startIndex, NUM_IMG) :
+    #Si falla al cargar la imagen comprueba que hay num_img suficientes en la carpeta
     x_test, img_test = aux.loadImage(IMG_PATH+folder, num_img, size= IMG_SIZE, unclassified_images=True, realID=realID, networkName=NetworkModelName)# Quitar unclassified_images y realID para imagenet
 #Si createImages = True: cargará las imagenes originales desde la carpeta y generará las adversarias de cero
 #Si unclassified_images = True: cargará las imagenes que no son de imagenet y por tanto no estan dentro de una carpeta con el valor de su ID
