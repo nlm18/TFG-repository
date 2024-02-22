f62e7088038a3bb440a6766d3cc3c85c7971791d
Anibal Pedraza <87324214+anibalpedraza@users.noreply.github.com>
2024-02-22 14:08:55 +0100

    Mejoras sobre la comprobacion para resumir los resultados en un unico fichero

--- matlab/calculateStatistics.m
+++ matlab/calculateStatistics.m
@@ -5,55 +5,66 @@ function calculateStatistics(dataPath,outputPath)
     networkList=networkList(3:end);
     metrics={'mediaIntensidadPixeles','varianza'};%'mediaIntensidadPixeles','ssim','varianza'};
 
+    % Define some global parameters
+    combinationPairs=[1,2;1,3;2,3];
+    ttestTableFull=table();
+    
     % Iterate over networks (only directories)
     for i=1:length(networkList)
         if networkList(i).isdir
             networkName=networkList(i).name;
             networkPath=fullfile(dataPath,networkName);
-            fprintf(['Processing network: ' networkName]);
+            fprintf(['Processing network: ',networkName,'\n']);
             % Iterate over metrics
             for j=1:length(metrics)
                 metricName=metrics{j};
                 metricFilePath=fullfile(networkPath,[networkName,'_',metricName,'.csv']);
-                disp(['Processing metric: ' metricName]);
+                fprintf(['\t','Processing metric: ',metricName,'\n']);
                 % Read data
                 tableData=readtable(metricFilePath,'ReadRowNames',true,'Delimiter',...
                     ';','DecimalSeparator',',');
                 nVars=size(tableData,2);
-                % Define pairs of combinations
+                % Define names of combinationPairs
                 datasetNames=tableData.Properties.RowNames;
-                combinations=[1,2;1,3;2,3];
-                combinationsNames=cell(size(combinations,1),1);
-                for k=1:size(combinations,1)
-                    combinationsNames{k}=[datasetNames{combinations(k,1)},'_vs_',datasetNames{combinations(k,2)}];
+                combinationsNames=cell(size(combinationPairs,1),1);
+                for k=1:size(combinationPairs,1)
+                    combinationsNames{k}=[datasetNames{combinationPairs(k,1)},'_vs_',datasetNames{combinationPairs(k,2)}];
                 end
                 % Empy table to store ttest results
-                ttestTable=table('Size',[size(combinations,1),2],'VariableTypes',{'double','double'},...
-                    'VariableNames',{'ttestT','ttestP'},'RowNames',combinationsNames);
-                % Iterate over combinations
-                for c=1:size(combinations,1)
-                    disp(['Processing combination: ',combinationsNames{c}]);
+                ttestTable=table('Size',[size(combinationPairs,1),5],'VariableTypes',...
+                    {'string','string','string','double','double'},...
+                    'VariableNames',{'network','metric','combination','ttestT','ttestP'});
+                % Iterate over combinationPairs
+                for c=1:size(combinationPairs,1)
+                    fprintf(['\t','\t','Processing combination: ',combinationsNames{c},'\n']);
                     % Plot with legend and no graphics
                     figure('Visible','off');
-                    scatter(1:nVars,tableData{combinations(c,1),:},'blue');
+                    scatter(1:nVars,tableData{combinationPairs(c,1),:},'blue');
                     hold on;
-                    scatter(1:nVars,tableData{combinations(c,2),:},'red');
-                    legend(datasetNames{combinations(c,1)},datasetNames{combinations(c,2)});
+                    scatter(1:nVars,tableData{combinationPairs(c,2),:},'red');
+                    legend(datasetNames{combinationPairs(c,1)},datasetNames{combinationPairs(c,2)});
                     % Save plot
                     plotPath=fullfile(outputPath,[networkName,'_',metricName,'_',...
                         combinationsNames{c},'.png']);
                     saveas(gcf,plotPath);
                     % Ttest
-                    [ttestT,ttestP]=ttest2(tableData{combinations(c,1),:},tableData{combinations(c,2),:});
+                    [ttestT,ttestP]=ttest2(tableData{combinationPairs(c,1),:},tableData{combinationPairs(c,2),:});
                     % Store ttest results in the table
-                    ttestTable{combinationsNames{c},'ttestT'}=ttestT;
-                    ttestTable{combinationsNames{c},'ttestP'}=ttestP;
+                    ttestTable{c,'network'}=string(networkName);
+                    ttestTable{c,'metric'}=string(metricName);
+                    ttestTable{c,'combination'}=string(combinationsNames{c});
+                    ttestTable{c,'ttestT'}=ttestT;
+                    ttestTable{c,'ttestP'}=ttestP;
                 end
-                % Write ttest results to file
-                ttestPath=fullfile(outputPath,[networkName,'_',metricName,'_ttestResults.csv']);
-                writetable(ttestTable,ttestPath,'WriteRowNames',true,'Delimiter',',');
+                ttestTableFull=[ttestTableFull;ttestTable];
             end
         end
     end
+    
+    % Write global ttest results to file
+    ttestPathFull=fullfile(outputPath,'ttestResultsFull.csv');
+    writetable(ttestTableFull,ttestPathFull,'Delimiter',',');
+    
+    fprintf('Finished processing all networks and metrics\n');
 
 end
